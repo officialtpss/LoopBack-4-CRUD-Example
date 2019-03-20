c06374f61de4bd22d77ef743f64a87fda39fd6ed
officialtpss <officialtpss@gmail.com>
2019-03-20 15:03:15 +0530

    # removed unused variable

--- src/keys.ts
+++ src/keys.ts
@@ -7,7 +7,6 @@ export namespace JWTAuthenticationBindings {
   export const STRATEGY = BindingKey.create<JWTStrategy>(
     'authentication.strategies.jwt.strategy',
   );
-  export const SECRET = BindingKey.create<string>('authentication.jwt.secret');
   export const SERVICE = BindingKey.create<JWTAuthenticationService>(
     'services.authentication.jwt.service',
   );
